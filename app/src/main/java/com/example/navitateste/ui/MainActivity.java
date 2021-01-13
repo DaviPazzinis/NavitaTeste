1a9e724b1c1308c357e0bb4b645fdb2b28a301c6
DaviPazzinis <davipazzinis@gmail.com>
2021-01-13 00:41:11 -0300

    Internet Connection Test

--- app/src/main/java/com/example/navitateste/ui/MainActivity.java
+++ app/src/main/java/com/example/navitateste/ui/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,19 +20,25 @@ import com.example.navitateste.adapter.MovieListAdapter;
 import com.example.navitateste.model.MovieResponseDTO;
 import com.example.navitateste.model.MovieModel;
 import com.example.navitateste.viewmodel.MovieListViewModel;
+import com.example.navitateste.viewmodel.NetworkChangeReceiver;
 
 public class MainActivity extends AppCompatActivity implements MovieListAdapter.OnNoteListener {
 
     private MovieResponseDTO movieResponseDTOList;
     private MovieListAdapter adapter;
 
+    BroadcastReceiver broadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        broadcastReceiver = new NetworkChangeReceiver();
+        registerNetworkBroadcastReceiver();
+
         RecyclerView recyclerView = findViewById(R.id.main_recyclerView);
-        TextView tvNoResult = findViewById(R.id.main_tv_noResult );
+        TextView tvNoResult = findViewById(R.id.main_tv_noResult);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -64,5 +75,30 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         MovieModel movieModel = movieResponseDTOList.getResults().get(position);
         it.putExtra("title", movieModel);
         startActivity(it);
+    }
+
+    protected void registerNetworkBroadcastReceiver() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+
+    }
+
+    protected void unregisterNetwork() {
+        try {
+            unregisterReceiver(broadcastReceiver);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterNetwork();
     }
 }
---
+++ app/src/main/java/com/example/navitateste/viewmodel/NetworkChangeReceiver.java
@@ -0,0 +1,35 @@
+package com.example.navitateste.viewmodel;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            if (isOnline(context)){
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context, "Not Connected", Toast.LENGTH_SHORT).show();
+            }
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isOnline(Context context){
+        try {
+            ConnectivityManager connectivityManager =  (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return (networkInfo!= null && networkInfo.isConnected());
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
